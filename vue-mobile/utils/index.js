f1912d6ccf656cb5c435f76099e2bcd5c179348a
TimofeevVadim <vtimofeev223@gmail.com>
2022-03-03 12:50:24 +0300

    Change "askOpenPgpKeyPassword" method

--- vue-mobile/utils/index.js
+++ vue-mobile/utils/index.js
@@ -4,9 +4,9 @@ export const askOpenPgpKeyPassword = (sFullEmail, getParentComponent, fCallback)
   console.log(oAppComponent, 'oAppComponent')
   const oAskOpenPgpKeyPasswordComponent = oAppComponent ? oAppComponent.$refs.AskOpenPgpKeyPassword : null
   console.log(oAskOpenPgpKeyPasswordComponent, 'oAskOpenPgpKeyPasswordComponent')
-  if (oAskOpenPgpKeyPasswordComponent.length) {
-    if (_.isFunction(oAskOpenPgpKeyPasswordComponent[0].askOpenPgpKeyPassword)) {
-      oAskOpenPgpKeyPasswordComponent[0].askOpenPgpKeyPassword(sFullEmail, fCallback)
+  if (oAskOpenPgpKeyPasswordComponent) {
+    if (_.isFunction(oAskOpenPgpKeyPasswordComponent.askOpenPgpKeyPassword)) {
+      oAskOpenPgpKeyPasswordComponent.askOpenPgpKeyPassword(sFullEmail, fCallback)
     }
   }
 }
